951b94523c214f5eb3a0866a280e619ee2a3f38f
Filiphe Vilar Figueiredo <liphvf@gmail.com>
2015-08-04 16:19:15 -0300

    optimaze pdf_compress

--- Scripts/pdf_compress.py
+++ Scripts/pdf_compress.py
@@ -2,7 +2,7 @@ import os
 import glob
 
 
-def pdf_compress(entrada=glob.glob('*.pdf')):
+def pdf_compress_convert(entrada=glob.glob('*.pdf')):
     '''
     Esta função pega todos os pdfs presente na entrada,  comprime,
     e joga em uma pasta output
@@ -14,5 +14,35 @@ def pdf_compress(entrada=glob.glob('*.pdf')):
 
         output_folder = 'output/'
         command = 'convert %s -compress Zip %s' % (pdf, output_folder + pdf)
+        # print(command)
+        os.system(command)
+
+def pdf_compress_gs(entrada=glob.glob('*.pdf')):
+    '''
+    Esta função pega todos os pdfs presente na entrada,  comprime,
+    e joga em uma pasta output
+    '''
+    if not os.path.exists('output'):
+        os.mkdir('output')
+
+    for pdf in entrada:
+
+        output_folder = 'output/'
+        command = 'gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/screen -dNOPAUSE -dBATCH  -dQUIET -sOutputFile=%s %s' % (output_folder + pdf, pdf)
+        # print(command)
+        os.system(command)
+
+def pdf_compress_qpdf(entrada=glob.glob('*.pdf')):
+    '''
+    Esta função pega todos os pdfs presente na entrada,  comprime,
+    e joga em uma pasta output
+    '''
+    if not os.path.exists('output'):
+        os.mkdir('output')
+
+    for pdf in entrada:
+
+        output_folder = 'output/'
+        command = 'qpdf --linearize %s %s' % (pdf,output_folder + pdf)
         # print(command)
         os.system(command)
